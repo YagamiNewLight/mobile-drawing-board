ba5a365eefda62d3ccaa7df6a1230b5c9a37d53e
Wangkezhi <1075049028@qq.com>
2017-10-12 00:50:48 +0800

    优化了下代码

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /**
  * Created by WKZ on 2017/7/20.
  */
+!function(){
 var prepageX;
 var prepageY;
 var myCanvas = document.createElement('canvas');
@@ -10,18 +11,11 @@ myCanvas.width = document.documentElement.clientWidth;
 myCanvas.globalCompositeOperation = 'source-atop';
 document.body.appendChild(myCanvas);
 
-function start1(e) {
-    var pageX = e.originalEvent.touches[0].pageX;
-    var pageY = e.originalEvent.touches[0].pageY;
-    prepageX = pageX;
-    prepageY = pageY;
+function start(e) {
+    prepageX = e.originalEvent.touches[0].pageX;
+    prepageY = e.originalEvent.touches[0].pageY;
 }
-function start2(e) {
-    var pageX = e.originalEvent.touches[0].pageX;
-    var pageY = e.originalEvent.touches[0].pageY;
-    prepageX = pageX;
-    prepageY = pageY;
-}
+
 function painting(e) {
     e.preventDefault();
     var pageX = e.originalEvent.touches[0].pageX;
@@ -36,41 +30,38 @@ function painting(e) {
     prepageY = pageY;
 }
 
-function eraser(e) {
-    e.preventDefault();
-}
-$(myCanvas).on('touchstart', start1);
-$(myCanvas).on('touchmove', painting);
-$('.pen').on('click', function () {
-    console.log(1);
-    $('.pen').parent().addClass('active');
-    $('.eraser').parent().removeClass('active');
-    $(myCanvas).on('touchstart', start1);
-    $(myCanvas).on('touchmove', painting);
-    $(myCanvas).off('touchstart',start2);
-    $(myCanvas).off('touchmove', eraseing);
-});
 function eraseing(e){
     e.preventDefault();
     var pageX = e.originalEvent.touches[0].pageX;
     var pageY = e.originalEvent.touches[0].pageY;
     canvasText.beginPath();
-    canvasText.lineWidth='20';
-    canvasText.strokeStyle='#fff';
+    canvasText.lineWidth = '10';
+    canvasText.strokeStyle='white';
     canvasText.moveTo(prepageX, prepageY);
     canvasText.lineTo(pageX, pageY);
     canvasText.stroke();
     prepageX = pageX;
     prepageY = pageY;
 }
-$('.eraser').on('click',function(){
-    $('.eraser').parent().addClass('active');
-    $('.pen').parent().removeClass('active');
-    $(myCanvas).on('touchstart',start2);
-    $(myCanvas).on('touchmove', eraseing);
-    $(myCanvas).off('touchstart',start1);
-    $(myCanvas).off('touchmove', painting);
-});
+
+
+function paintOrErase(selectorOn,selectorOff,fnOn,fnOff){
+    $(selectorOn).on('click',function(){
+    $(selectorOn).parent().addClass('active');
+    $(selectorOff).parent().removeClass('active');
+    $(myCanvas).on('touchstart',start);
+    $(myCanvas).on('touchmove', fnOn);
+    $(myCanvas).off('touchmove', fnOff);
+    })
+}
+
+paintOrErase('.eraser','.pen',eraseing,painting);
+paintOrErase('.pen','.eraser',painting,eraseing);
+
+$(myCanvas).on('touchstart', start);
+$(myCanvas).on('touchmove', painting);
+
+
 $('.icons').on('touchmove',function(e){
     e.originalEvent.preventDefault();
 });
@@ -78,14 +69,14 @@ $('.qingkong').on('click',function(){
     canvasText.clearRect(0,0,myCanvas.width,myCanvas.height);
     $('.eraser').parent().removeClass('active');
     $('.pen').parent().addClass('active');
-    $(myCanvas).on('touchstart', start1);
+    $(myCanvas).on('touchstart', start);
     $(myCanvas).on('touchmove', painting);
-    $(myCanvas).off('touchstart',start2);
     $(myCanvas).off('touchmove', eraseing);
 });
 $('.camera').on('click',function(e){
     e.originalEvent.preventDefault();
     var data = myCanvas.toDataURL('image/png');
-    var w=window.open('about:blank','image from canvas');
+    var w = window.open('about:blank','image from canvas');
     w.document.write("<img src='"+data+"' alt='from canvas'/>");
 })
+}();
